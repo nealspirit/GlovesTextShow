40869e83360582d7c1cd13f6fc5b74075be59925
nealspirit <15168220524@163.com>
2019-05-12 21:06:44 +0800

    加入语音

--- app/src/main/java/com/glovestextshow/android/MainActivity.java
+++ app/src/main/java/com/glovestextshow/android/MainActivity.java
@@ -2,12 +2,92 @@ package com.glovestextshow.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechUtility;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText ip;
+    private TextView text;
+    public String SpeechText;
+
+    Socket socket = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ip = findViewById(R.id.edit_ip);
+        text = findViewById(R.id.show_message);
+        SpeechUtility.createUtility(MainActivity.this, SpeechConstant.APPID +"=5cd5813b");
+
+        findViewById(R.id.button_ip).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                connect();
+            }
+        });
+
+        findViewById(R.id.button_send).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                send();
+            }
+        });
+    }
+
+    private void connect() {
+        ip.setText(NetWorkUtils.getLocalIpAddress(MainActivity.this));
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ServerSocket serverSocket = new ServerSocket(3000);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this,"端口打开",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    while (true){
+                        Socket socket = serverSocket.accept();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(MainActivity.this,"有客户端连接到了本机",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        new ServerTask(socket,text).execute();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void disconnect(){
+        if (socket!= null){
+            try {
+                socket.close();
+                Toast.makeText(MyApplication.getContext(),"端口关闭",Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void send() {
+        SpeechUtils.speekText(text.getText().toString());
     }
 }
---
+++ app/src/main/java/com/glovestextshow/android/SpeechUtils.java
@@ -0,0 +1,58 @@
+package com.glovestextshow.android;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechError;
+import com.iflytek.cloud.SpeechSynthesizer;
+import com.iflytek.cloud.SynthesizerListener;
+
+public class SpeechUtils {
+    public static void speekText(String textSpeech) {
+        SpeechSynthesizer mTts = SpeechSynthesizer.createSynthesizer(MyApplication.getContext(),null);
+        mTts.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant. TYPE_CLOUD); //设置云端
+        mTts.startSpeaking(textSpeech,new MySynthesizerListener());
+    }
+
+    public static class MySynthesizerListener implements SynthesizerListener {
+        @Override
+        public void onSpeakBegin() {
+            Toast.makeText(MyApplication.getContext(),"开始播放",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onBufferProgress(int i, int i1, int i2, String s) {
+
+        }
+
+        @Override
+        public void onSpeakPaused() {
+            Toast.makeText(MyApplication.getContext(),"暂停播放",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onSpeakResumed() {
+            Toast.makeText(MyApplication.getContext(),"继续播放",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onSpeakProgress(int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onCompleted(SpeechError speechError) {
+            if (speechError == null){
+                Toast.makeText(MyApplication.getContext(),"播放完毕",Toast.LENGTH_SHORT).show();
+            }else if (speechError != null){
+                Toast.makeText(MyApplication.getContext(),speechError.getPlainDescription(true),Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        public void onEvent(int i, int i1, int i2, Bundle bundle) {
+
+        }
+    }
+}
