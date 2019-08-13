622e31bab8abcc43fea355497132c936d9544a3f
nealspirit <15168220524@163.com>
2019-08-13 18:40:03 +0800

    添加语句库v1.0

--- app/src/main/java/com/glovestextshow/android/utils/HandLanguageUtils.java
+++ app/src/main/java/com/glovestextshow/android/utils/HandLanguageUtils.java
@@ -18,18 +18,25 @@ public class HandLanguageUtils {
             "保持","联系",
             "谢谢",
             "我","爱",
-            "再见","好"};
+            "再见","好",
+            "中国","计量","大学","欢迎","参观",
+            "加","聊天",
+            "微信"};
     //语句库
-    private static String[] wordsLibrary = {"你好","你电话多少","你很可爱","你要加油","你很高兴","你电话什么","你多少钱",
+    private static String[] wordsLibrary = {"你好","你电话多少","你很可爱","你要加油","你很高兴","你电话什么","这个东西多少钱",
             "很高兴见到你",
             "对不起",
             "这个地址怎么走",
             "保持联系",
             "谢谢",
-            "我爱你","我很高兴","我要加油","跟我走","我联系你",
+            "我爱你","我要加油","我联系你",
             "再见",
             "我很可爱",
-            "好"};
+            "好的",
+            "欢迎来到中国计量大学参观",
+            "这是我的联系方式",
+            "加我大学",
+            "上面有我的微信二维码"};
 
     public static String identityString(String text){
         int N = wordsLibrary.length;
@@ -48,12 +55,8 @@ public class HandLanguageUtils {
                break;
            }
         }
-
-        if(wordsLibrary[index].equals("你多少钱")){
-            wordsLibrary[index] = "这个东西多少钱";
-        }
-        if(wordsLibrary[index].equals("好")){
-            wordsLibrary[index] = "好的";
+        if (wordsLibrary[index].equals("加我大学")){
+            wordsLibrary[index] = "你可以和我聊天";
         }
 
         return wordsLibrary[index];
